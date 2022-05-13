97a1dfdc090b9bfc65ff8f310a4afc3ab24c2ed9
Satory <sarasatorynb@gmail.com>
2022-05-13 11:28:55 +0200

    Day 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const moviesList = require('./data/movies.json')
+const moviesList = require('./data/movies.json');
 
 // create and config server
 const server = express();
@@ -18,3 +18,9 @@ server.get('/movies', (req, res) => {
   const response = moviesList;
   res.json(response);
 });
+
+const staticServerPathWeb = './src/public-react'; // En esta carpeta ponemos los ficheros estáticos - La ruta es relativa desde la raiz del proyecto (servidor).
+server.use(express.static(staticServerPathWeb));
+
+const staticServerPathWebImg = './src/public-movies-images'; // En esta carpeta ponemos los ficheros estáticos - La ruta es relativa desde la raiz del proyecto (servidor).
+server.use(express.static(staticServerPathWebImg));
