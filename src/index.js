9456dbd0137d30e31839b972d2fa3d60d6ad1749
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-05-19 11:29:12 +0200

    Day 5

--- src/index.js
+++ src/index.js
@@ -41,6 +41,58 @@ server.get('/movies', (req, res) => {
   res.json(response);
 });
 
+
+
+server.post('/sign-up', (req, res) =>{
+  console.log('sucedo?', req.body);
+  
+})
+
+/*
+cÓDIGO de dayana
+
+// endpoint para añadir alguna fila 
+server.post('/add', (req, res) => {
+  // variables para simular los datos de la user
+  const name = 'Tal';
+  const brief = 'Cual';
+  const img = 'URL';
+  const desc = 'Mec';
+
+  // declaración para buscar en DB
+  const selectWomen = db.prepare('SELECT id FROM women name = ?');
+
+  // .get para ejecutar la sentencia SQL con la variable name
+  const foundWomen = selectWomen.get(name);
+
+  // condición para comprobar si existe o no en DB
+  if (foundWomen === undefined) {
+    // declaración para insertar en DB
+    const query = db.prepare(
+      'INSERT INFO women (description, name, photo, brief) VALUE (?,?,?,?)'
+    );
+
+    // .run para ejecutar la sentencia SQL con las variables que simulan datos de la user
+    const insertWomen = query.run(desc, name, image, brief);
+
+    res.json({
+      success: true,
+      // ole esto siguiente:
+      userId: insertWomen.lastInsertRowid,
+    });
+  } else {
+    res.json({
+      success: false,
+      msj: 'La mujer ya existe',
+    });
+  }
+});
+
+
+*/
+
+
+
 const staticServerPathWeb = './src/public-react'; // En esta carpeta ponemos los ficheros estáticos - La ruta es relativa desde la raiz del proyecto (servidor).
 server.use(express.static(staticServerPathWeb));
 
